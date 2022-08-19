6f07c036e526975beab05cb5144a3518ad1994cb
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-08-19 21:06:02 +0100

    updated main class functionality

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
         System.out.println("Changed!");
 
+        // This is a test to see if I understand Git and GitHub
+        System.out.println("This was written on the desktop!");
+
     }
 
 }
